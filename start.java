97e9f607db91ae2a331f63e7b72aac6af50cc69f
Hanke5 <hanke.husmann@t-online.de>
2026-09-21 09:03:16 +0200

    Add files via upload
    Signed-off-by: Hanke5 <hanke.husmann@t-online.de>

--- start.java
+++ start.java
@@ -1,4 +1,5 @@
-
+import java.awt.Desktop;
+import java.net.URI;
 public class start
 {
     public start()
@@ -6,11 +7,28 @@ public class start
         double age;
         String Name;
         String answer;
-        date Geburtsdatum;
-        Geburtsdatum = EinAusgabe;
         age = EinAusgabe.ganzzahlEingabe("Sachma Dein Alter (in Jahren)");
         Name = EinAusgabe.textEingabe("NAME");
-        answer = "Du heisst "+Name+" und dein Alter ist "+age / 2+" zwei Jahres Schritte";
-        EinAusgabe.benachrichtige("", answer);
+        answer = "Du bist gut genug";
+        if (age>=18)
+           {
+               EinAusgabe.benachrichtige("", answer);
+               try {
+               String url = "https://youtu.be/0GnA8VYOfko";
+               Desktop desktop = Desktop.getDesktop();
+               
+               // Prüfen, ob das Öffnen unterstützt wird
+               if (Desktop.isDesktopSupported() && desktop.isSupported(Desktop.Action.BROWSE)) {
+                desktop.browse(new URI(url));
+               }
+           }
+           catch (Exception e) {
+            e.printStackTrace();
+           }
+           }
+        else
+        {
+            EinAusgabe.benachrichtige("","Du bist zu Jung");
+        }
     }
 }
